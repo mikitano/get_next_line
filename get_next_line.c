acd6e9221fa2349ca0627f9e5acfea5acd4c83f9
Magali Midori Kitano <mkitano@c3r3p5.42sp.org.br>
2025-08-23 18:06:29 -0300

    gnl.c att, still no full function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mkitano <mkitano@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 14:43:47 by mkitano           #+#    #+#             */
-/*   Updated: 2025/08/23 16:51:27 by mkitano          ###   ########.fr       */
+/*   Updated: 2025/08/23 18:01:39 by mkitano          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@ char	*get_next_line(int fd)
 	static char	*remaining;
 	char		*line;
 	char		*buffer;
+
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+		return (NULL);
+	if ((fd < 0) | (BUFFER_SIZE <= 0))
+	{
+		free (buffer);
+		remaining = NULL;
+		return (NULL);
+	}
+	line = read_line(fd, remaining, buffer);
+}
+
+char	*read_line(int fd, char *remaining, char *buffer)
+{
 	
 }
